7dbfab8adf6cdf5e2872c64a7bd296e78166a825
Alex Curran <alexcurran.va@gmail.com>
2021-05-05 11:41:49 -0500

    Increment/decrement btns working in Counter comp

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+
+  function incrementHandler() {
+    dispatch({ type: "increment" });
+  }
+
+  function decrementHandler() {
+    dispatch({ type: "decrement" });
+  }
 
   function toggleCounterHandler() {
 
@@ -13,6 +22,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
